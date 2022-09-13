4cc99705727d4da28cf3bab0e240ee10845e737d
Alex <example@gmail.com>
2022-09-13 23:35:16 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -250,21 +250,11 @@ const data = [{
   descr: 'А вот и мой следущий подарок. Он избавит тебя от навязанных толпами невежд качеств. Во тьме ты сможешь узреть истинную красоту и уродство...',
   composition: 'Кулирка : 92% Лайкра : 8%'
 }, {
-  name: 'Supressed Feelings',
+  name: 'Unformed Darkness',
   price: 'Цена 3',
   hoverSrc: 'img/tshirt_hover.png',
   mainSrc: 'img/tshirt_main.png',
   tagline: 'Текст 3',
-  colection: 'The Ascendance of Darkness',
-  modalSrc: ['img/tshirt_main.png', 'img/tshirt_main.png', 'img/tshirt_main.png'],
-  descr: 'Чтобы сохранить статус в обществе ты привык гасить переполняющия тебя чувства. Прими мой третий подарок и сделай выбор: выплеснуть весь океан этих эмоций или же навсегда от них отказаться. В любом случае тьма способна удовлетворить твой выбор...',
-  composition: 'Кулирка : 92% Лайкра : 8%'
-}, {
-  name: 'Unformed Darkness',
-  price: 'Цена 4',
-  hoverSrc: 'img/tshirt_hover.png',
-  mainSrc: 'img/tshirt_main.png',
-  tagline: 'Текст 4',
   colection: 'The Ascendance of Darkness',
   modalSrc: ['img/tshirt_hover.png', 'img/tshirt_hover.png', 'img/tshirt_hover.png'],
   descr: 'Теперь ты готов к моему последнему подарку. Прими его и приблизься к совершенству. Но помни о главном - тьма может принимать любую форму. Теперь иди и погрузи ещё больше людей в нашу веру...',
@@ -758,6 +748,10 @@ function animateBtn() {
       behavior: "smooth"
     });
   });
+  const logo = document.querySelector('.titlebox__logotype');
+  setTimeout(function () {
+    logo.classList.add('titlebox__logotype-active');
+  }, 1500);
 }
 
 /* harmony default export */ __webpack_exports__["default"] = (animateBtn);
