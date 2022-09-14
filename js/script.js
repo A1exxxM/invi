7deaeb5b77281dd723c438af8c0db16927716c41
Alex <example@gmail.com>
2022-09-14 17:53:58 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -230,33 +230,33 @@ function showDescr() {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 const data = [{
-  name: 'Fool"s Desperation',
+  name: "Fool's Desperation",
   price: 'Цена 1',
-  hoverSrc: 'img/tshirt_hover.png',
-  mainSrc: 'img/tshirt_main.png',
+  hoverSrc: "img/Fool's Desperation/2.jpg",
+  mainSrc: "img/Fool's Desperation/1.jpg",
   tagline: 'Текст 1',
   colection: 'The Ascendance of Darkness',
-  modalSrc: ['img/tshirt_hover.png', 'img/tshirt_main.png', 'img/tshirt_main.png'],
+  modalSrc: ["img/Fool's Desperation/1.jpg", "img/Fool's Desperation/2.jpg", "img/Fool's Desperation/3.jpg"],
   descr: 'Глупость - вершина человеческого общетва. В неведении легче жить и не замечать бесполезности бытия.Вот мой первый подарок - познай же красоту знаний, дарованную тьмой...',
   composition: 'Кулирка : 92% Лайкра : 8%'
 }, {
   name: 'Decepton Beauty',
   price: 'Цена 2',
-  hoverSrc: 'img/tshirt_hover.png',
-  mainSrc: 'img/tshirt_main.png',
+  hoverSrc: "img/Deception Beauty/2.jpg",
+  mainSrc: "img/Deception Beauty/1.png",
   tagline: 'Текст 2',
   colection: 'The Ascendance of Darkness',
-  modalSrc: ['img/tshirt_main.png', 'img/tshirt_main.png', 'img/tshirt_hover.png'],
+  modalSrc: ["img/Deception Beauty/1.png", "img/Deception Beauty/2.jpg", "img/Deception Beauty/3.jpg"],
   descr: 'А вот и мой следущий подарок. Он избавит тебя от навязанных толпами невежд качеств. Во тьме ты сможешь узреть истинную красоту и уродство...',
   composition: 'Кулирка : 92% Лайкра : 8%'
 }, {
   name: 'Unformed Darkness',
   price: 'Цена 3',
-  hoverSrc: 'img/tshirt_hover.png',
-  mainSrc: 'img/tshirt_main.png',
+  hoverSrc: 'img/Unformed Darkness/3.jpg',
+  mainSrc: 'img/Unformed Darkness/1.png',
   tagline: 'Текст 3',
   colection: 'The Ascendance of Darkness',
-  modalSrc: ['img/tshirt_hover.png', 'img/tshirt_hover.png', 'img/tshirt_hover.png'],
+  modalSrc: ['img/Unformed Darkness/1.png', 'img/Unformed Darkness/2.jpg', 'img/Unformed Darkness/3.jpg'],
   descr: 'Теперь ты готов к моему последнему подарку. Прими его и приблизься к совершенству. Но помни о главном - тьма может принимать любую форму. Теперь иди и погрузи ещё больше людей в нашу веру...',
   composition: 'Кулирка : 92% Лайкра : 8%'
 }];
